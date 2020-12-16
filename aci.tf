175a61a94f8bfd1b1b3ed1c6cd4ca6a7b394c81a
Camillo <camrossi@cisco.com>
2020-12-16 16:34:55 +1100

    test2

--- aci.tf
+++ aci.tf
@@ -21,9 +21,3 @@ resource "aci_tenant" "test-tenant" {
   name        = "test-tenant"
   description = "This tenant is created by terraform"
 }
-
-resource "aci_app_profile" "test-app" {
-  tenant_dn   = aci_tenant.test-tenant.id
-  name        = "test-app"
-  description = "This app profile is created by terraform"
-}
