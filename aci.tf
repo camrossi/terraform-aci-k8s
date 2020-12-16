a519731adc919a8e42f596bf4bdf0d239dbecb8e
Camillo <camrossi@cisco.com>
2020-12-16 18:27:08 +1100

    test

--- aci.tf
+++ aci.tf
@@ -11,41 +11,41 @@ terraform {
  }
 }
 
-provider "aci" {
-  # cisco-aci user name
-  username = "admin"
-  # cisco-aci password
-  password = "123Cisco123"
-  # cisco-aci url
-  url      = "http://fab2-apic1.cam.ciscolabs.com/"
-  insecure = true
-}
-
-resource "aci_tenant" "tenant" {
-  name        = var.tenant
-  description = "This tenant is created by terraform"
-}
-
-resource "aci_application_profile" "ap" {
-  tenant_dn  = aci_tenant.tenant.id
-  name       = var.application
-}
-
-resource "aci_application_epg" "demoepg" {
-  application_profile_dn = aci_application_profile.ap.id
-  name = var.epg
-  flood_on_encap = "disabled"
-  relation_fv_rs_bd = "uni/tn-KubeSpray/BD-aci-containers-KubeSpray-pod-bd"
-  relation_fv_rs_prov = ["uni/tn-KubeSpray/brc-aci-containers-KubeSpray-health-check"]
-  relation_fv_rs_cons = ["uni/tn-common/brc-KubeSpray-l3out-allow-all",
-                        "uni/tn-KubeSpray/brc-aci-containers-KubeSpray-dns", 
-                        "uni/tn-KubeSpray/brc-aci-containers-KubeSpray-icmp", 
-                        "uni/tn-KubeSpray/brc-aci-containers-KubeSpray-istio"]
-}
+#provider "aci" {
+#  # cisco-aci user name
+#  username = "admin"
+#  # cisco-aci password
+#  password = "123Cisco123"
+#  # cisco-aci url
+#  url      = "http://fab2-apic1.cam.ciscolabs.com/"
+#  insecure = true
+#}
+#
+#resource "aci_tenant" "tenant" {
+#  name        = var.tenant
+#  description = "This tenant is created by terraform"
+#}
+#
+#resource "aci_application_profile" "ap" {
+#  tenant_dn  = aci_tenant.tenant.id
+#  name       = var.application
+#}
+#
+#resource "aci_application_epg" "demoepg" {
+#  application_profile_dn = aci_application_profile.ap.id
+#  name = var.epg
+#  flood_on_encap = "disabled"
+#  relation_fv_rs_bd = "uni/tn-KubeSpray/BD-aci-containers-KubeSpray-pod-bd"
+#  relation_fv_rs_prov = ["uni/tn-KubeSpray/brc-aci-containers-KubeSpray-health-check"]
+#  relation_fv_rs_cons = ["uni/tn-common/brc-KubeSpray-l3out-allow-all",
+#                        "uni/tn-KubeSpray/brc-aci-containers-KubeSpray-dns", 
+#                        "uni/tn-KubeSpray/brc-aci-containers-KubeSpray-icmp", 
+#                        "uni/tn-KubeSpray/brc-aci-containers-KubeSpray-istio"]
+#}
 
 # Same parameters as kubernetes provider
 provider "kubernetes" {
-  config_path = "./"
+  config_path = "./kube_config"
 }
 
 resource "kubernetes_namespace" "ns" {
