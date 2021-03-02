c4adedf77406760a20efce003a3bf242725be736
Camillo <camrossi@cisco.com>
2021-03-02 13:22:24 +1100

    test

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "frontend_service" {
-  value = data.kubernetes_service.guestbook.id
+  value = data.kubernetes_service.guestbook.load_balancer_ingress[0].ip
   description = "The public IP address of the Service"
 }
